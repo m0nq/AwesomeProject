eed66a5751da2fdae66b1246ae5fec6228d53a07
m0nq <forgetmenaught@gmail.com>
2024-03-04 12:18:28 -0800

    Set up basic list elements for rendering boxes of color

---
+++ Components/ColorBox.js
@@ -0,0 +1,30 @@
+import { Text } from 'react-native';
+import { View } from 'react-native';
+import { StyleSheet } from 'react-native';
+
+const ColorBox = ({ color, text }) => {
+  return (
+    <View style={[container, box, { backgroundColor: color }]}>
+      <Text
+        style={[boxText, { color: parseInt(color.replace('#', ''), 16) > 0xffffff / 1.1 ? 'black' : 'white' }]}>{text}: {color}</Text>
+    </View>
+  );
+};
+
+const { container, box, boxText } = StyleSheet.create({
+  container: {
+    margin: 5,
+    paddingVertical: 10,
+    paddingHorizontal: 10
+  },
+  box: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  boxText: {
+    color: 'white',
+    fontWeight: 'bold'
+  }
+});
+
+export default ColorBox;
